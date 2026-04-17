eaac1153627912b6109fbbd54d39fb2e2e2783df
AMARDEEP KUMAR <157851224+Amardeep095@users.noreply.github.com>
2026-04-17 21:02:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -89,5 +89,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀  Server running on port ${PORT}`);
 });
