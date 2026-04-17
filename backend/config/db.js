e0738ec5866e0f2452a0adf6f168e479a57db5bb
AMARDEEP KUMAR <157851224+Amardeep095@users.noreply.github.com>
2026-04-17 21:32:00 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,10 +6,9 @@ const connectDB = async () => {
   if (isConnected) return;
 
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 5000,
     });
-
     isConnected = true;
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
